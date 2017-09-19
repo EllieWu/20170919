7626383c9d4dbc90be5d196b8e29e087c4c60997
EllieWu <a0981262519@gmail.com>
2017-09-19 11:14:03 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("學號:106022078");
+        System.out.println("系級:行動商務與多媒體應用一年級");
+        System.out.println("姓名:吳沛蓁");
+        System.out.println("時間:2017/09/19");
+        System.out.println("地點:\"I319\"");
+    }
+}
